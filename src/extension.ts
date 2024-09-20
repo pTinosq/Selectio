e0d3a8af93305746b9dd7b24bc00c49d1ee5212d
pTinosq <tinosps@gmail.com>
2024-09-20 13:20:00 +0100

    Set up basic structure for toggling selectio mode

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,69 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let isTypeMode = false;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "selectio" is now active!');
+  function setTypeMode(value: boolean) {
+    isTypeMode = value;
+    vscode.commands.executeCommand("setContext", "selectio.isTypeMode", value);
+  }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('selectio.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Selectio!');
-	});
+  const selectioTypeDisposable = vscode.commands.registerCommand(
+    "type",
+    (args) => {
+      if (!isTypeMode) {
+				vscode.commands.executeCommand('default:type', args);
+        return;
+      }
 
-	context.subscriptions.push(disposable);
+      console.log("051772", isTypeMode);
+      const editor = vscode.window.activeTextEditor;
+      const char: string = args.text;
+
+      switch (char) {
+        case "\r": // "return" key (Enter)
+          console.log("return");
+          break;
+        case "\u001b": // "escape" key (Esc)
+          console.log("escape");
+          break;
+        default:
+          console.log("xxx:", char);
+          break;
+      }
+    }
+  );
+
+  context.subscriptions.push(selectioTypeDisposable);
+
+  const selectioTypeModeDisposable = vscode.commands.registerCommand(
+    "extension.startSelectioTypeMode",
+    () => {
+      setTypeMode(true);
+      console.log("type");
+    }
+  );
+
+  context.subscriptions.push(selectioTypeModeDisposable);
+
+  const exitSelectioTypeModeDisposable = vscode.commands.registerCommand(
+    "extension.exitSelectioTypeMode",
+    () => {
+      setTypeMode(false);
+      console.log("exit");
+    }
+  );
+
+  context.subscriptions.push(exitSelectioTypeModeDisposable);
+
+  const confirmSelectioTypeModeDisposable = vscode.commands.registerCommand(
+    "extension.confirmSelectioTypeMode",
+    () => {
+      console.log("confirm");
+    }
+  );
+
+  context.subscriptions.push(confirmSelectioTypeModeDisposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
